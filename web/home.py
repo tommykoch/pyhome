b5feddabdd10de8ac748d0896ad25d092b4354e1
tommykoch <tommykoch@gmail.com>
2016-05-16 11:04:42 +0200

    added temperature sensor updated requirements for w1thermsensor

--- web/home.py
+++ web/home.py
@@ -11,6 +11,12 @@ elro_pin =17
 motionfile = "motion.txt"
 # enable webcam support
 webcam = True
+# enable temperature sensor (if library is available)
+try:
+    import w1thermsensor
+    temperature = w1thermsensor.W1ThermSensor()
+except ImportError:
+    temperature = None
 
 
 @app.route('/')
@@ -23,12 +29,15 @@ def status():
     import os, time
     now = datetime.datetime.now()
     timeString = now.strftime("%Y-%m-%d %H:%M")
-    details  = { 'temperature':  21.5,  # TODO
-                       'time': timeString   # update time
-    }
+    details = {'time': timeString}   # update time
+    # read temperature (if sensor is available)
+    if temperature:
+        tempcelsius = temperature.get_temperature()
+        details.update({'temperature': tempcelsius})
     # read status of PIR sensor
     if os.path.exists(motionfile):
         details['motion'] = time.ctime(os.path.getmtime(motionfile))
+    # take snapshot of webcam (max 1 per minute)
     if webcam:
         import sh, os
         timeString = now.strftime("%Y%m%d_%H%M")
@@ -36,7 +45,8 @@ def status():
         snapshot = os.path.join("static", "webcam", snapshot)
         details['webcam'] = '/'+snapshot
         snapshot = os.path.abspath(snapshot)
-        sh.fswebcam("--title", "Home", "--save", snapshot)
+        if not os.path.exists(snapshot):
+            sh.fswebcam("--title", "Home", "--save", snapshot)
     return jsonify(**details)
 
 
@@ -51,16 +61,14 @@ def turn(switch):
     return jsonify(**details)
 
 
-def  turnSwitch(switch, on=False):
+def turnSwitch(switch, on=False):
     from elro import  RemoteSwitch
     devices = { 'A': 1, 'B': 2, 'C': 4, 'D': 8, 'E':16 }
     device=devices.get(switch, 0)
     if not device:
         return False
     # print "turn switch '%s' to '%s' - device#%d"  % (switch, "on" if on else "off", device)
-    device = RemoteSwitch(device,
-                                             key=elro_key,
-                                             pin=elro_pin)
+    device = RemoteSwitch(device, key=elro_key, pin=elro_pin)
     if on:
         device.switchOn()
     else:
