1de5d2bb67eda00d2c6d15336e231a9d67105fdd
Thomas Koch <koch@orbiteam.de>
2020-02-16 22:13:48 +0100

    cleanup

--- web/home.py
+++ web/home.py
@@ -1,28 +1,34 @@
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 import datetime
+from alarm import AlarmService
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+app.config.from_pyfile('config.py')
 
-# config data
-elro_key = [1,0,0,0,1]
-# change the pin(s) accpording to your wiring
-elro_pin =17
 # status file for motion detection
 motionfile = "motion.txt"
-# enable webcam support
-webcam = True
+
 # enable temperature sensor (if library is available)
 try:
     import w1thermsensor
     temperature = w1thermsensor.W1ThermSensor()
 except ImportError:
     temperature = None
+except:
+    temperature = None
 
 
 @app.route('/')
 def index():
-    return redirect('/static/home/index.html')
-
+    return render_template('/index.html',
+                           webcam = app.config["WEBCAM"],
+                           sensor = app.config["SENSOR"],
+                           temperature = app.config["TEMPERATURE"],
+                           alarm = app.config["ALARM"],
+                           # alarmOn = True,
+                           switches = {'A': app.config["SWITCH_A"],
+                           'B': app.config["SWITCH_B"],
+                           'C': app.config["SWITCH_C"]})
 
 @app.route("/status", methods=["GET"])
 def status():
@@ -31,14 +37,14 @@ def status():
     timeString = now.strftime("%Y-%m-%d %H:%M")
     details = {'time': timeString}   # update time
     # read temperature (if sensor is available)
-    if temperature:
+    if temperature and app.config["TEMPERATURE"]:
         tempcelsius = temperature.get_temperature()
         details.update({'temperature': tempcelsius})
     # read status of PIR sensor
     if os.path.exists(motionfile):
         details['motion'] = time.ctime(os.path.getmtime(motionfile))
     # take snapshot of webcam (max 1 per minute)
-    if webcam:
+    if app.config["WEBCAM"]:
         import sh, os
         timeString = now.strftime("%Y%m%d_%H%M")
         snapshot = "snapshot-%s.jpg" % timeString
@@ -49,6 +55,24 @@ def status():
             sh.fswebcam("--title", "Home", "--save", snapshot)
     return jsonify(**details)
 
+@app.route("/alarm/<on>", methods=["POST"])
+def alarm(on):
+    details  = {}
+    if turnAlarm(on=="on"):
+        now = datetime.datetime.now()
+        timeString = now.strftime("%Y-%m-%d %H:%M")
+        details.update({'time': timeString})
+    return jsonify(**details)
+
+def turnAlarm(on):
+    """write command into file - pass enable/disable command"""
+    print ("turn alarm on? %s" % on)
+    enable = on
+    disable = not on
+    alarm = AlarmService()
+    alarm.load()
+    alarm.save(enable, disable)
+    return True
 
 @app.route("/turn/<switch>", methods=["POST"])
 def turn(switch):
@@ -60,7 +84,6 @@ def turn(switch):
         details.update({'time': timeString})
     return jsonify(**details)
 
-
 def turnSwitch(switch, on=False):
     from elro import  RemoteSwitch
     devices = { 'A': 1, 'B': 2, 'C': 4, 'D': 8, 'E':16 }
@@ -68,7 +91,9 @@ def turnSwitch(switch, on=False):
     if not device:
         return False
     # print "turn switch '%s' to '%s' - device#%d"  % (switch, "on" if on else "off", device)
-    device = RemoteSwitch(device, key=elro_key, pin=elro_pin)
+    device = RemoteSwitch(device,
+                          key=app.config["ELRO_KEY"],
+                          pin=app.config["ELRO_PIN"])
     if on:
         device.switchOn()
     else:
@@ -77,4 +102,6 @@ def turnSwitch(switch, on=False):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0',
+            port=app.config["HTTP_PORT"],
+            debug=app.config["DEBUG"])
